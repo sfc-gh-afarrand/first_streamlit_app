1c6fab0648f9dc9de16b278f16420ea4d561b403
sfc-gh-afarrand <111505787+sfc-gh-afarrand@users.noreply.github.com>
2023-02-27 10:00:23 +0000

    Update streamlit_app.py
    Add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
+# Import Libraries
 import streamlit
 
+
+# Menu
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header("Breakfast Favourites")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
+
+
+# BUILD SMOOTHIE
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
